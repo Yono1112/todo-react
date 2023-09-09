3f1722374c6849fa22e0efe632e7e9bb46df91e1
Yuma Ohno <100353174+Yono1112@users.noreply.github.com>
2023-09-09 07:20:37 +0000

    finish section5-52 adn 5-53

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,31 @@ import React from "react"
 import "./styles.css"
 
 export const App = () => {
-    return (
-        <>
-            <h1>test</h1>
-            <p>helllo</p>
-        </>
-    );
+	return (
+		<>
+			<div className="input-area">
+				<input placeholder="TODO"/>
+				<button>add</button>
+			</div>
+			<div className="incomplete-area">
+				<p className="title">incomplete TODO</p>
+				<ul>
+					<div className="list-row">
+						<li>aaaaaa</li>
+						<button>finish</button>
+						<button>delete</button>
+					</div>
+				</ul>
+			</div>
+			<div className="complete-area">
+				<p className="title">complete TODO</p>
+				<ul>
+					<div className="list-row">
+						<li>bbbbbb</li>
+						<button>return</button>
+					</div>
+				</ul>
+			</div>
+		</>
+	);
 };
