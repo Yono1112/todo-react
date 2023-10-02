75efd41fc22aeceaa6a230a4e216503537d7eca1
Yuma Ohno <100353174+Yono1112@users.noreply.github.com>
2023-10-02 03:12:35 +0000

    chnage style-in-js in Inputarea

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
+const style = {
+	backgroundColor: '#c1ffff',
+	width: '400px',
+	height: '30px',
+	padding: '8px', /*上下左右の余白*/
+	margin: '8px', /*外側に対する余白*/
+	borderRadius: '8px', /*角を丸くする*/
+}
+
 export const InputTodo = (props) => {
 	const { todoText, onChangeTodoText, onClickTodoText} = props;
 	return (
-		<div className="input-area">
+		<div style={style}>
 			<input placeholder="TODO" value={todoText} onChange={onChangeTodoText}/>
 			<button onClick={onClickTodoText}>add</button>
 		</div>
